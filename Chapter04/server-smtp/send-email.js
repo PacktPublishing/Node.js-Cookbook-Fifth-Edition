b9e100c853334b31084db376254c5feee2e08bf6
Beth Griggs <bethanyngriggs@gmail.com>
2024-02-02 04:56:03 +0000

    Chapter04: Creating an SMTP server

---
+++ Chapter04/server-smtp/send-email.js
@@ -0,0 +1,21 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'localhost',
+  port: 4321
+});
+
+transporter.sendMail(
+  {
+    from: 'beth@example.com',
+    to: 'laddie@example.com',
+    subject: 'Hello',
+    text: 'Hello world!'
+  },
+  (err, info) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Message Sent:', info);
+  }
+);
