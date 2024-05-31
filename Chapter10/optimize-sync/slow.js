d25540e5e1a8fe78b254f26b1d62afbba424b0ee
Beth Griggs <bethanyngriggs@gmail.com>
2024-05-31 03:11:53 +0100

    Chapter10: Optimizing sync recipe code

---
+++ Chapter10/optimize-sync/slow.js
@@ -0,0 +1,15 @@
+function sumOfSquares (maxNumber) {
+  const array = Array.from(Array(maxNumber + 1).keys());
+
+  return array
+
+    .map((number) => {
+      return number ** 2;
+    })
+
+    .reduce((accumulator, item) => {
+      return accumulator + item;
+    });
+}
+
+module.exports = sumOfSquares;
