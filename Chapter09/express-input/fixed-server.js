bf60f9c60259bb67f29199e10b57935e23347860
Beth Griggs <bethanyngriggs@gmail.com>
2024-07-01 03:32:01 +0100

    Chapter09: add Anticipating Malicious Input recipe code

---
+++ Chapter09/express-input/fixed-server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  asyncWork(() => {
+    let msg = req.query.msg;
+    if (Array.isArray(msg)) msg = msg.pop();
+    const upper = (msg || '').toUpperCase();
+    res.send(upper);
+  });
+});
+
+const asyncWork = (callback) => {
+  setTimeout(callback, 0);
+};
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
---
+++ Chapter09/express-input/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  asyncWork(() => {
+    const upper = (req.query.msg || '').toUpperCase();
+    res.send(upper);
+  });
+});
+
+const asyncWork = (callback) => {
+  setTimeout(callback, 0);
+};
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
