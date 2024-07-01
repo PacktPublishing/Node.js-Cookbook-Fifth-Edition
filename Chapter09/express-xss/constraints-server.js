3b8835e802ddf939e7d3a49d71789097b489957a
Beth Griggs <bethanyngriggs@gmail.com>
2024-07-01 04:20:05 +0100

    Chapter09: update constraints-server recipe code

--- Chapter09/express-xss/constraints-server.js
+++ Chapter09/express-xss/constraints-server.js
@@ -1,27 +1,42 @@
 const express = require('express');
-
 const app = express();
 
 app.get('/', (req, res) => {
   const { previous, lang, token } = req.query;
 
+  if (!validateParameters({ previous, token, lang }, req.query)) {
+    res.sendStatus(422);
+    return;
+  }
+
   getServiceStatus((status) => {
-    res.send(` 
-      <h1>Service Status</h1> 
-      <div id=status> 
-        ${status} 
-      </div> 
-      <div> 
-      <a href="${previous}${token}/${lang}">Back</a> 
-      </div> 
+    res.send(`
+      <h1>Service Status</h1>
+      <div id=status>
+        ${status}
+      </div>
+      <div>
+      <a href="${previous}${token}/${lang}">Back</a>
+      </div>
     `);
   });
 });
 
 const getServiceStatus = (callback) => {
   const status = 'All systems are running.';
-
   callback(status);
+};
+
+const validateParameters = ({ previous, token, lang }, query) => {
+  return (
+    Object.keys(query).length <= 3 &&
+    typeof lang === 'string' &&
+    lang.length === 2 &&
+    typeof token === 'string' &&
+    token.length === 16 &&
+    typeof previous === 'string' &&
+    previous.length <= 16
+  );
 };
 
 app.listen(3000, () => {
