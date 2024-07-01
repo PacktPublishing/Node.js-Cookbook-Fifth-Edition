525751439a4380fb2ca63bfd2774ad1538882abe
Beth Griggs <bethanyngriggs@gmail.com>
2024-07-01 03:46:57 +0100

    Chapter09: add Preventing JSON Pollution recipe code

---
+++ Chapter09/json-pollution/fixed-server.js
@@ -0,0 +1,59 @@
+const http = require('http');
+
+const { STATUS_CODES } = http;
+const Ajv = require('ajv');
+const ajv = new Ajv();
+
+const schema = {
+  title: 'Greeting',
+  type: 'object',
+  properties: {
+    msg: { type: 'string' },
+    name: { type: 'string' }
+  },
+  additionalProperties: false,
+  required: ['msg']
+};
+
+const validate = ajv.compile(schema);
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/') {
+    greeting(req, res);
+
+    return;
+  }
+
+  res.statusCode = 404;
+  res.end(STATUS_CODES[res.statusCode]);
+});
+
+const greeting = (req, res) => {
+  let data = '';
+
+  req.on('data', (chunk) => (data += chunk));
+
+  req.on('end', () => {
+    try {
+      data = JSON.parse(data);
+    } catch (e) {
+      res.end('');
+      return;
+    }
+
+    if (!validate(data, schema)) {
+      res.end('');
+      return;
+    }
+
+    if (data.hasOwnProperty('name')) {
+      res.end(`${data.msg} ${data.name}`);
+    } else {
+      res.end(data.msg);
+    }
+  });
+};
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
---
+++ Chapter09/json-pollution/server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+
+const { STATUS_CODES } = http;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/') {
+    greeting(req, res);
+
+    return;
+  }
+
+  res.statusCode = 404;
+  res.end(STATUS_CODES[res.statusCode]);
+});
+
+const greeting = (req, res) => {
+  let data = '';
+
+  req.on('data', (chunk) => (data += chunk));
+
+  req.on('end', () => {
+    try {
+      data = JSON.parse(data);
+    } catch (e) {
+      res.end('');
+      return;
+    }
+
+    if (data.hasOwnProperty('name')) {
+      res.end(`${data.msg} ${data.name}`);
+    } else {
+      res.end(data.msg);
+    }
+  });
+};
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
