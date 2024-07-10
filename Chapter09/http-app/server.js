aee4d1cddaa1726106c2acf3db0c203d896b85f8
Beth Griggs <bethanyngriggs@gmail.com>
2024-07-10 15:14:23 +0100

    Chapter09: update recipe code

--- Chapter09/http-app/server.js
+++ Chapter09/http-app/server.js
@@ -6,14 +6,17 @@ const server = http.createServer((req, res) => {
 });
 
 const secureHeaders = (res) => {
-  res.setHeader('X-DNS-Prefetch-Control', 'off');
-  res.setHeader('Expect-CT', 'max-age=0');
-  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
-  res.setHeader('X-Download-Options', 'noopen');
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Permitted-Cross-Domain-Policies', 'none');
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Resource-Policy', 'same-origin');
+  res.setHeader('Origin-Agent-Cluster', '?1');
   res.setHeader('Referrer-Policy', 'no-referrer');
-  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('Strict-Transport-Security', 'max-age=15552000; includeSubDomains');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-DNS-Prefetch-Control', 'off');
+  res.setHeader('X-Download-Options', 'noopen');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('X-Permitted-Cross-Domain-Policies', 'none');
+  res.setHeader('X-XSS-Protection', '0');
 };
 
 server.listen(3000, () => {
